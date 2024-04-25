b8c12bd94605c4caf97a977d142d05b92c2c8eca
DumplingDefender <messiharden1@gmail.com>
2024-04-25 15:29:32 -0500

    help me

--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -6,7 +6,7 @@ function Hero() {
     <div className="h-[100vh] bg-blue-200 ">
       <div className="">
         <img
-          class="invisible md:visible md:mt-[3%] z-0 w-full animated-image"
+          class="invisible md:visible md:mt-[3%] z-0 w-full pulse"
           src={heroImg1}
           alt="Image Description"
         />
